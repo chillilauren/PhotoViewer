f0b2f0dbb601546ef69d935eb04c9d24b38db308
Chilli Mahendra <chillilauren@live.co.uk>
2020-07-30 12:09:13 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { PhotoViewer } from './PhotoViewer/PhotoViewer';
+
+function App() {
+  return (
+    <div>
+      <h1>React Photo Viewer</h1>
+      <PhotoViewer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PhotoViewer/PhotoViewer.js
@@ -0,0 +1,15 @@
+// import React
+import React from "react";
+
+// declare new function
+function PhotoViewer() {
+    return (
+        // return JSX
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+// export function so that it can be called elsewhere
+export {PhotoViewer};
